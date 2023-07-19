a7d895bfb0a48771e4394267abdc84b902981101
Dilay Kirisci <isik.kirisci@gmail.com>
2023-07-19 16:26:48 +0300

    product and footer

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import Home from "./pages/Home";
+
+import ProductList from "./pages/ProductList";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Cart from "./pages/Cart";
+import Product from "./pages/Product";
+
+const App = () => {
+	return <Product />;
+};
+
+export default App;
